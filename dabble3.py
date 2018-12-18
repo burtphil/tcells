3b2a5eec9e7fe158531616be542d49bf744dbc5a
Philipp Burt <philippburt@googlemail.com>
2018-12-18 16:28:55 +0100

    changed dabble3 is now curr working script

--- dabble3.py
+++ dabble3.py
@@ -27,6 +27,12 @@ def gamma_cdf(t, alpha, beta):
     return gammainc(alpha,dummy)      
 
 def make_cells(ncells, nsteps, nstates):
+    """
+    returns an array of cells for given number of
+    desired cells, steps and cell states
+    initializes random numbers probability
+    of cell state transition
+    """
     cells = np.zeros((ncells, nsteps, nstates))
     for i in range(cells.shape[0]):        
         # random number for th0 to th_eff transition
@@ -36,75 +42,71 @@ def make_cells(ncells, nsteps, nstates):
 def exp_cdf(t, mean):
     return 1 - np.exp(-t / mean)
 
-def add_cell(cells):
-    """
-    insert a new naive cell into cell vector if there are dead cells available
-    otherwise append a new naive cell to cell vector
-    """
-    ncells, nsteps, nstates = cells.shape
-
-    # initialize new cell
-    new_cell = make_cells(1, nsteps, nstates)    
-    dead_cell_idx = 1
-    # check if there are any dead cells
-    c = cells[:,:,0] == dead_cell_idx   
-    if c.any():
-        # if yes, insert new cell into dead cell vector
-        d = np.where(cells[:,:,0] == dead_cell_idx)[0][0]
-        #print new_cell
-        cells[d,:,:] = new_cell        
-    # else append new cell
-    else:
-        cells = np.concatenate((cells, new_cell))
-    
-    return cells
-
 def stoc_simulation(start, stop, nsteps, ncells, nstates):
     """
     run a simulation of the th1 th2 models   
     this function uses the a cumulative gamma fct integral to calculate transition probability
     """
-    cells = make_cells(ncells, nsteps, nstates)
+    # initialize some dummy dead cells t
+    n_dummies = 50
+    cells = make_cells(n_dummies+ncells, nsteps, nstates)
     time = np.linspace(start, stop, nsteps)
     
+    # make some dead cells in the beginning
+    th0_idx = 0
+    dead_cell_idx = 1
+    cells[:n_dummies,:,0] = dead_cell_idx
+    
+    # initialize random number and probability for birth process
     rnd_birth = np.random.rand()
     p_birth = 0
     t0 = 0
+    
+    # loop over each time point
     for i in range(len(time)-1):
         t = time[i]
         t_new = time[i+1]
 
-        # I think if I add a new cell, it should not be totally new for all timesteps
-        if rnd_birth > p_birth:           
-            p_birth = p_birth + (exp_cdf(t_new-t0, 1.)-exp_cdf(t-t0, 1.))
-        else:
-            if t == 0:
-                print "hi"
-            p_birth = 0
-            t0 = t
-            rnd_birth = np.random.rand()
-            cells = add_cell(cells)              
-        # cell for each time step
+        # get all cells for current time step
         cell_j = cells[:,i,:]
-                
+
+        # cell number should be number of alive cells not total number              
         cell_number = cell_j.shape[0]
-           
+        
+        # get number of current th0 cells
+        n_th0 = sum(cell_j[:,0] == th0_idx)
+        
+        # loop over each cell for current time point
         for j in range(cell_number):
-            cell = cell_j[j,:]                        
+            cell = cell_j[j,:] 
+                       
             #check if cell dies
-            if cell[0] == 0:
+            if cell[0] == th0_idx:
                 if cell[2] > cell[1]:
-                    cell[1] = cell[1]+ (exp_cdf(t_new, cell_number)-exp_cdf(t, cell_number))
+                    cell[1] = cell[1]+ (exp_cdf(t_new, n_th0)-exp_cdf(t, n_th0))
                 else:
-                    cell[0] = 1
-              
+                    cell[0] = dead_cell_idx
                     
             cells[j,i+1,:] = cell
-        
-         
-        
-        #check if a new cell is added
             
+        # is there a birth? if not, cumulatively add p_birth
+        if rnd_birth > p_birth:           
+            p_birth = p_birth + (exp_cdf(t_new-t0, 1.)-exp_cdf(t-t0, 1.))
+        
+        # if there is a birth, draw new rnd number 
+        # and make a new th0 cell out of a dead cell
+        else:            
+            p_birth = 0
+            t0 = t
+            rnd_birth = np.random.rand()            
+            #search for a dead cell to transform             
+            c = cells[:,i,0] == dead_cell_idx
+            assert c.any()
+            # note that np.where returns a tuple
+            cell_idx = np.where(cells[:,i,0] == dead_cell_idx)[0][0]
+            #print cell_idx
+            cells[cell_idx, i+1,:] = make_cells(1,1, nstates)
+        
     return [cells, time]     
 
 def get_cells(cells, time):
@@ -127,15 +129,17 @@ def get_cells(cells, time):
     return th1_cells, dead_cells
 
 #==============================================================================
-# set up params and run
+# set up params
 #==============================================================================
 start = 0
-stop = 5
-nsteps = 2000
-ncells = 1000
+stop = 10
+nsteps = 20000
+ncells = 200
 nstates = 3
 nsim = 20
-
+# =============================================================================
+# run simulation
+# =============================================================================
 th1_cells = np.zeros_like(np.linspace(start, stop, nsteps))
 
 simulation = [stoc_simulation(start, stop, nsteps, ncells, nstates) for i in range(nsim)]
@@ -152,6 +156,9 @@ df.colums = ["Th1" for _ in range(nsim)]
 df["time"] = time
 df_long = df.melt(id_vars = ["time"])
 
+# =============================================================================
+# plot figure
+# =============================================================================
 fig, ax = plt.subplots(1, 1, figsize =(5,4))
 stoc_plot = sns.lineplot(x = "time", 
                          y = "value", 
@@ -160,6 +167,7 @@ stoc_plot = sns.lineplot(x = "time",
                          ax = ax,
                          legend = False)
 ax.set_ylabel("$n_{cells}$")
-ax.set_title(str(nsteps)+" steps, "+str(nsim)+ "simulations")
+ax.set_title(str(nsteps)+" steps, "+str(nsim)+" simulations")
+ax.set_xlim(0,time[-1])
 plt.tight_layout()
 fig.savefig("stoc_simulation.pdf", bbox_inches = "tight")
